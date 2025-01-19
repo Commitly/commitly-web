f2f1324af76cbf40bcd8b72461c85b972923a2cd
dlrjsgml44 <leegeh1213@gmail.com>
2025-01-19 23:44:58 +0900

    feat: 토큰 재발급

--- src/utils/TokenIntercepter.ts
+++ src/utils/TokenIntercepter.ts
@@ -11,13 +11,51 @@ axiosInstance.interceptors.request.use(
     const accessToken = localStorage.getItem('accessToken'); // 로컬 스토리지에서 액세스 토큰 가져오기
     console.log('accessToken:', accessToken);
     // /login/oauth2/code/github 경로 제외하고 Authorization 헤더 추가
-    if (accessToken && !config.url?.includes('/login/oauth2/code/github')) {
+    if (accessToken && !config.url?.includes('/login/oauth2/code/github') && !config.url?.includes('/login/refresh')) {
       config.headers['Authorization'] = accessToken;
     }
 
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      console.log('Access Token 만료, Refresh Token으로 재발급 시도...');
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (!refreshToken) {
+        console.error('Refresh Token 없음. 로그인 페이지로 이동합니다.');
+        window.location.href = '/';
+        return Promise.reject(error);
+      }
+
+      try {
+        // Refresh Token으로 새 Access Token 발급 요청
+        const { data } = await axios.post('http://localhost:8080/auth/refresh', {
+          refreshToken,
+        });
+
+        // 새 Access Token 저장
+        localStorage.setItem('accessToken', data);
+        console.log('새 Access Token 발급 성공:', data);
+
+        // 원래 요청 재시도
+        error.config.headers['Authorization'] = data.accessToken;
+        return axiosInstance(error.config);
+      } catch (refreshError) {
+        console.error('Refresh Token도 만료됨. 로그인 페이지로 이동합니다.');
+        localStorage.clear();
+        window.location.href = '/';
+        return Promise.reject(refreshError);
+      }
+    }
+
     return Promise.reject(error);
   }
 );
