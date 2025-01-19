c0eccb65e9e4192532ea3837230c35ab4008df51
dlrjsgml44 <leegeh1213@gmail.com>
2025-01-19 13:48:52 +0900

    feat: HomePage

---
+++ src/pages/home/HomePage.style.ts
@@ -0,0 +1,12 @@
+import exp from 'constants';
+import styled from 'styled-components';
+
+const S = {
+    container: styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    `,
+}
+
+export default S;
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import S from './HomePage.style';
+
+function HomePage() {
+  return (
+    <S.container>
+        <h1>ㅎㅇㅎㅇ</h1>
+    </S.container>
+  );
+}
+
+export default HomePage;
