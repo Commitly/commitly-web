89cedb4f409ec9f5af2a19f6ed4ef2eba3516d78
dlrjsgml44 <leegeh1213@gmail.com>
2025-01-23 19:41:11 +0900

    feat: gpt 한테 요청하는 모달

---
+++ src/components/modal/GPTModalComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function GPTModalComponent() {
+    return (
+        <div>
+            <h1>GPTModalComponent</h1>
+        </div>
+    )
+}
+
+export default GPTModalComponent;
