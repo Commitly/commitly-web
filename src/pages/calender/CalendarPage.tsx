4d8208f8bd574ae0493bc086c8e67c4babc15b30
dlrjsgml44 <leegeh1213@gmail.com>
2025-01-24 14:16:50 +0900

    feat: 캘린더 커스텀

--- src/pages/calender/CalendarPage.tsx
+++ src/pages/calender/CalendarPage.tsx
@@ -2,12 +2,10 @@ import React, { useState, useRef } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import GPTModalComponent from '../../components/modal/GPTModalComponent';
-import { Day } from '../../types/day/Day';
-import { display, height, minHeight } from '@mui/system';
+import './CustomCalendar.css';
+
 
 const style = {
   position: 'absolute',
@@ -29,13 +27,13 @@ function CalendarPage() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  
+
   const oneDayPlus = (date: Date) => {
     const newDate = new Date(date);
     newDate.setDate(date.getDate() + 1);
     return newDate;
   }
-  
+
   const onClickDay = (value: Date, event: React.MouseEvent) => {
     onChange(value);  // 날짜 값을 변경
     handleOpen();
@@ -45,8 +43,12 @@ function CalendarPage() {
     <Box>
       <Calendar
         onClickDay={onClickDay}
+        formatDay={(locale, date) => date.toLocaleString('en', { day: 'numeric' })}
         value={value}
-        calendarType="gregory" 
+        calendarType="gregory"
+        minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+        maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+      className={'custom-calendar'}
       />
       <Modal
         open={open}
@@ -54,11 +56,9 @@ function CalendarPage() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        
-          <GPTModalComponent date={
-            oneDayPlus(value)
-            }/>
-        
+
+        <GPTModalComponent date={oneDayPlus(value)} />
+
       </Modal>
     </Box>
   )
