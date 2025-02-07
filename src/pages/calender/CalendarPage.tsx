9b8acf9a6f3634f09b93e6acfbc15069f14c275d
dlrjsgml44 <leegeh1213@gmail.com>
2025-02-07 20:18:22 +0900

    feat: 커밋 기록이 있는 날짜는 표시해주기

--- src/pages/calender/CalendarPage.tsx
+++ src/pages/calender/CalendarPage.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import GPTModalComponent from '../../components/modal/GPTModalComponent';
 import './CustomCalendar.css';
-
+import { List } from '@mui/material';
+import axiosInstance from '../../utils/TokenIntercepter';
+import axios from 'axios';
 
 const style = {
   position: 'absolute',
@@ -22,16 +24,32 @@ const style = {
 };
 
 function CalendarPage() {
+  const [markDate, setMarkDate] = useState<String[]>([]);
   const [value, onChange] = useState(new Date());
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
   const oneDayPlus = (date: Date) => {
     const newDate = new Date(date);
     newDate.setDate(date.getDate() + 1);
     return newDate;
   }
+  useEffect(() => {
+    axiosInstance.get('/reto/get')
+      .then((response) => {
+        const updatedMarkDates = response.data.map((item: any) => {
+          const date = new Date(item.retoDate); // 날짜 객체로 변환
+          date.setDate(date.getDate() - 1); // 하루를 뺀 날짜 설정
+          return date.toISOString().split("T")[0]; // YYYY-MM-DD 형식으로 반환
+        });
+        setMarkDate(updatedMarkDates);
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
 
   const onClickDay = (value: Date, event: React.MouseEvent) => {
     onChange(value);  // 날짜 값을 변경
@@ -45,7 +63,10 @@ function CalendarPage() {
         formatDay={(locale, date) => date.toLocaleString('en', { day: 'numeric' })}
         value={value}
         calendarType="gregory"
-        
+        tileClassName={({ date }) => {
+          const formattedDate = date.toISOString().split("T")[0]; // YYYY-MM-DD 형식
+          return markDate.includes(formattedDate) ? "highlight-text" : "";
+        }}
         minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
         maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
       className={'custom-calendar'}
